8d144e6a834627380bbda3b21a33ac848af0fab4
juwon0718 <juwon0718@naver.com>
2026-02-07 10:14:03 +0900

    [fix] LocalDateTime timezone issue

--- src/main/kotlin/com/team10/instagram/domain/story/repository/StoryRepository.kt
+++ src/main/kotlin/com/team10/instagram/domain/story/repository/StoryRepository.kt
@@ -134,7 +134,7 @@ class StoryRepository(
                 id = rs.getLong("story_id"),
                 userId = rs.getLong("user_id"),
                 imageUrl = rs.getString("image_url"),
-                createdAt = rs.getTimestamp("created_at").toLocalDateTime(),
+                createdAt = rs.getTimestamp("created_at").toLocalDateTime().minusHours(9), // to rollback NOW() (UTC timezone)
                 viewCount = rs.getInt("view_count"),
             )
         }
